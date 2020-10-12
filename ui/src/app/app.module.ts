4e7f88173d84792d9b5bcdb81a9be8265ba737ad
Rashmi Yadav <yads.rash@gmail.com>
2020-10-12 18:28:36 +0530

    added card

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { FilterComponent } from './filter/filter.component';
+import { BestsellerComponent } from './bestseller/bestseller.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainPageComponent,
+    FilterComponent,
+    BestsellerComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
